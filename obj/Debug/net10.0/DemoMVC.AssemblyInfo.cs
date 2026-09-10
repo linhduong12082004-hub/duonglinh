d57f69f612258bed3814b3889f48b923b5989ceb
TranDuongThuyLinh <linhduong12082004@gmail.com>
2026-09-10 13:36:28 +0700

    Add DemoMVC project

--- obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+839c48a329c3c594327ec68084c5b4088720c8b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d21d27fb6feaab155eef1acb04bd026e138b0c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
